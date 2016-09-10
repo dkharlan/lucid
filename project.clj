fd6cdbcf7219ef5fb2717c3bd87429e4db39dfae
Dave Harlan <david.k.harlan@gmail.com>
2016-09-10 15:49:19 -0400

    Upgrade Clojure version to 1.9.0-alpha12 (for core.spec), add clj-time dependency, add alembic dev dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject lucid "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :description "A flexible MUD framework written in Clojure"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
+                 [clj-time "0.12.0"]]
   :main ^:skip-aot lucid.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[alembic "0.3.2"]]}})
+
