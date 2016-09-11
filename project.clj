94257cbc1e229747ab29c2785a5e2d4de1952f64
Dave Harlan <david.k.harlan@gmail.com>
2016-09-11 14:05:20 -0400

    Add Datomic free edition to dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "A flexible MUD framework written in Clojure"
   :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
                  [clj-time "0.12.0"]
-                 [reduce-fsm "0.1.4"]]
+                 [reduce-fsm "0.1.4"]
+                 [com.datomic/datomic-free "0.9.5394"]]
   :main ^:skip-aot lucid.core
   :target-path "target/%s"
   :repl-options {:init (do
