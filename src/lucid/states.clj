74585e4a946497490c1ddaa37666b9c7bd64214f
Dave Harlan <david.k.harlan@gmail.com>
2016-09-10 21:48:18 -0400

    Verify that reduce-fsm will meet requirements for server state flow

--- src/lucid/states.clj
+++ src/lucid/states.clj
@@ -1,17 +1,30 @@
 (ns lucid.states
   (:require [reduce-fsm :refer [defsm-inc] :as fsm]))
 
-(defn character-exists? [[accumulator character-name]]
-  (println character-name)
-  (some #(= character-name %) ["Bob" "Billy" "Joe"]))
+(defn character-exists? [[_ character-name]]
+  (and
+    (string? character-name)
+    (some #(= character-name %) ["Bob" "Billy" "Joe"])))
+
+(defn password-is-valid? [[{{:keys [character-name]} :login} password]]
+  (let [name->password {"Bob" "foobar"
+                        "Billy" "12345"
+                        "Joe" "changeme"}]
+    (= password (get name->password character-name))))
 
 (defn- add-character-name [accumulator input current-state next-state]
-  accumulator)
+  (assoc-in accumulator [:login :character-name] input))
 
 (defsm-inc login
   [[:awaiting-name
-    [_ :guard character-exists?] -> {:action add-character-name} :derp]
-   [:derp
-    [_] -> :derp]]
+    [[_ #"^[A-z]+$"] :guard character-exists?] -> {:action add-character-name} :awaiting-password]
+   [:awaiting-password
+    [_ :guard password-is-valid?] -> :logged-in
+    [_ :guard #(not (password-is-valid? %))] -> :zombie]
+   [:logged-in
+    [[_ "quit"]] -> :zombie
+    [_] -> :logged-in]
+   [:zombie
+    [_] -> :zombie]]
   :dispatch :event-acc-vec)
 
