860818d7022ee05f5ad044f2752b0e64cedc9c38
Dave Harlan <david.k.harlan@gmail.com>
2016-09-17 14:36:43 -0400

    Add TODO notes

--- src/lucid/server.clj
+++ src/lucid/server.clj
@@ -8,12 +8,20 @@
   (swap! streams-atom dissoc stream-id)
   (log/debug "Connection from" (:remote-addr info) "closed"))
 
-(defn handle-input! [stream-id bytes]
+(defn handle-input! [stream-id bytes] ;; TODO need states-atom
   (let [message (-> bytes
                   (String. "UTF-8")
                   (clojure.string/trim))]
-    (log/debug "Message from" (str stream-id ":") message)))
+    (log/debug "Message from" (str stream-id ":") message)
+    ;; TODO feed message, derefed streams into fsm (retrieved from states-atom)
+    ;; TODO for each side effect:
+    ;; TODO    if stream side effect, write to target stream
+    ;; TODO    if db side effects, transact
+    ;; TODO swap! states dissoc side effects
+    ))
 
+;; TODO need to create a server stream w/ a fixed thread executor with size 1, then
+;; TODO all input messages go to this.
 (defn accept-new-connection! [streams-atom stream info]
   (let [stream-id (uuid/v1)]
     (log/debug "Accepting new connection with details" info)
