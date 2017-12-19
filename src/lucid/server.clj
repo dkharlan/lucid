c03877b9cf440ba916c2e478408d4ca5a5606b3c
Dave Harlan <david.k.harlan@gmail.com>
2017-12-18 23:30:07 -0500

    Fix infinite loop on socket stream closed; fixed hanging accept-new-connection!; make drained updater-signal stop the update-thread; add TODO notes for future work

--- src/lucid/server.clj
+++ src/lucid/server.clj
@@ -7,7 +7,8 @@
             [lucid.util :as util]))
 
 ;; TODO will also need to keep track of connection state(s)
-(defrecord Descriptor [id stream])
+(defn make-descriptor [id stream]
+  {:id id :stream stream})
 
 (defprotocol StatefulServer
   (start! [server-info])
@@ -21,7 +22,7 @@
     @tcp-server
     (log/info "TCP socket server has been started.")
     this)
-  (stop! [{:keys [tcp-server update-thread]}]
+  (stop! [{:keys [tcp-server updater-signal]}]
     (doseq [{:keys [stream]} @descriptors]
       (s/close! stream))
     (.close @tcp-server)
@@ -35,44 +36,54 @@
   (swap! descriptor dissoc descriptor-id)
   (log/debug "Connection from" (:remote-addr info) "closed"))
 
+(defn make-message [descriptor-id message]
+  {:descriptor-id descriptor-id :message (util/bytes->string message)})
+
+;; TODO messages are always []. why?
 (defn accept-new-connection! [descriptors message-buffer stream info]
   (log/debug "New connection initiated:" info)
-  (let [descriptor-id (uuid/v1)]
+  (let [descriptor-id       (uuid/v1)
+        make-message*       (partial make-message descriptor-id)
+        insert-into-buffer! #(s/put! message-buffer (make-message* %))]
     (s/on-closed stream (partial close! descriptors descriptor-id info))
-    (s/connect-via
-      stream
-      #(s/put! message-buffer 
-         {:descriptor-id descriptor-id :message (util/bytes->string %)})
-      message-buffer)
-    (swap! descriptors assoc (->Descriptor {:id descriptor-id :stream stream}))
+    (s/connect-via stream insert-into-buffer! message-buffer)
+    (swap! descriptors assoc descriptor-id (make-descriptor descriptor-id stream))
     (log/info "Accepted new connection from descriptor" descriptor-id "at" (:remote-addr info))))
 
 ;; TODO see if there's an easier / more succint / more idiomatic way to do this
 (defn- collect! [stream]
   (loop [messages []]
-    (let [message @(s/try-take! stream nil 0 ::nothing)]
-      (if (= message ::nothing)
+    (let [message @(s/try-take! stream nil 0 ::nothing)] 
+      (if (or (nil? message) (= message ::nothing)) 
         messages
-        (recur (conj messages messages))))))
+        (do
+          (log/debug "Message was" message)
+          (recur (conj messages messages)))))))
 
-;; TODO replace sleep with something more robust
 (defn update! [message-buffer updater-signal]
   (log/info "Update thread started.")
-  (while (not= ::shutdown @(s/try-take! updater-signal nil 0 ::nothing))
-    (let [messages (collect! message-buffer)]
-      (doseq [{:keys [descriptor-id message] :as msg} messages]
-        (log/debug "Message:" msg)
-        (log/info descriptor-id "says" message)))
-    (Thread/sleep 100)) 
+  (let [signal @(s/try-take! updater-signal nil 0 ::nothing)]
+    (while (and (not= ::shutdown signal) (not (nil? signal))) 
+      (let [messages (collect! message-buffer)]
+        (doseq [{:keys [descriptor-id message] :as msg} messages]
+          ;; TODO input processing logic goes here
+          (log/debug "Message:" msg)
+          (log/info descriptor-id "says" message)))
+      ;; TODO replace with something more robust
+      (Thread/sleep 100)))
   ;; TODO any cleanup goes here
   (log/info "Update thread finished cleaning up."))
 
 (defn make-server [port]
   (let [descriptors    (atom {})
-        message-buffer (s/buffered-stream 1000) ;; TODO may have to fiddle with the buffer length
+
+        ;; TODO closes when upstream is closed... fix
+        ;; TODO may have to fiddle with the buffer length
+        message-buffer (s/buffered-stream 1000)
+
         acceptor       (partial accept-new-connection! descriptors message-buffer)
         tcp-server     (delay (tcp/start-server acceptor {:port port}))
-        updater-signal (s/stream) ;; TODO what properties does this stream need?
+        updater-signal (s/stream) ;; TODO what properties does this stream need? could see a buffer being necessary
         updater        (partial update! message-buffer updater-signal)
         update-thread  (Thread. updater "update-thread")]
     (->Server descriptors message-buffer acceptor tcp-server update-thread updater-signal)))
