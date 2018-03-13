e8634d538ae6b76ea6f7e8d8bd81273d8fff8b8e
Dave Harlan <david.k.harlan@gmail.com>
2018-03-12 22:49:50 -0400

    Add client that connects to the echo server and creates a var in lucid.core.client to expose the channel to the REPL

--- src/cljs/lucid/client/core.cljs
+++ src/cljs/lucid/client/core.cljs
@@ -1,4 +1,19 @@
-(ns lucid.client.core)
+(ns lucid.client.core
+  (:require [chord.client :refer [ws-ch]]
+            [cljs.core.async :refer [>! <! put! close! go go-loop]]))
 
-(.log js/console "hello world")
+(go
+  (let [{:keys [ws-channel error]} (<! (ws-ch "ws://localhost:8080/connect"
+                                         {:format :str}))]
+    (if error
+      (console.error "Failed to establish websocket connection:" error)
+      (do
+        (def channel ws-channel) ;; for the REPL
+        (go-loop []
+          (let [{:keys [message error]} (<! ws-channel)]
+            (if error
+              (console.error error)
+              (do
+                (console.log "Server said:" message)
+                (recur)))))))))
 
