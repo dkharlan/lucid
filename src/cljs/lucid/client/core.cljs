f8c8acb34cba6f4989025d479c402c86177e36d5
Dave Harlan <david.k.harlan@gmail.com>
2018-03-12 23:09:54 -0400

    Refactor lucid.client.core

--- src/cljs/lucid/client/core.cljs
+++ src/cljs/lucid/client/core.cljs
@@ -2,20 +2,29 @@
   (:require [chord.client :refer [ws-ch]]
             [cljs.core.async :refer [>! <! put! close! go go-loop]]))
 
-(go
-  (let [{:keys [ws-channel error]} (<! (ws-ch "ws://localhost:8080/connect"
-                                         {:format :str}))]
-    (if error
-      (console.error "Failed to establish websocket connection:" error)
-      (do
-        (def channel ws-channel) ;; for the REPL
-        (go-loop []
-          (if-let [value (<! ws-channel)]
-            (let [{:keys [message error]} value]
-              (if error
-                (console.error error)
-                (do
-                  (console.log "Server said:" message)
-                  (recur))))
-            (console.log "Connection closed.")))))))
+(def websocket-connection-endpoint "ws://localhost:8080/connect")
+
+(defn message-loop! [channel]
+  (go-loop []
+    (if-let [value (<! channel)]
+      (let [{:keys [message error]} value]
+        (if error
+          (console.error error)
+          (do
+            (console.log "Server said:" message)
+            (recur))))
+      (console.log "Connection closed."))))
+
+(defn connect! [url]
+  (go
+    (let [{:keys [ws-channel error]} (<! (ws-ch url {:format :str}))]
+      (if error
+        (console.error "Failed to establish websocket connection:" error)
+        (do
+          (def channel ws-channel) ;; TODO remove me later; for REPL access
+          (message-loop! ws-channel)))))) 
+
+(.addEventListener js/document "DOMContentLoaded"
+  (fn [_]
+    (connect! websocket-connection-endpoint)))
 
