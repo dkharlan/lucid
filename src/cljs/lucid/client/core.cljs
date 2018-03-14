cd72f2dcb7d36a6d1319b30e1b97a177b9ab9f51
Dave Harlan <david.k.harlan@gmail.com>
2018-03-13 22:09:32 -0400

    Add desired styles and behavior for basic HTML client

--- src/cljs/lucid/client/core.cljs
+++ src/cljs/lucid/client/core.cljs
@@ -32,27 +32,30 @@
             (message-loop! ws-channel handler!)))))
     sender-fn-channel))
 
+(defn scroll-to-bottom! [textarea]
+  (if textarea
+    (set! (.-scrollTop textarea) (.-scrollHeight textarea))))
+
 (defn output-console [{:keys [buffer]}]
-  [:div
-   (map-indexed
-     (fn [index line]
-       ^{:key index} [:p line])
-     @buffer)])
+  [:div {:class "content"}
+   [:textarea
+    {:class "output-console zero-fill"
+     :read-only true
+     :ref scroll-to-bottom!
+     :value (apply str (interpose "\n" @buffer))}]]) ;; TODO doubt this is performant
 
 (defn input-line [{:keys [send-message!]}]
   (letfn [(enter-handler [event]
             (if (= (-> event .-keyCode) 13)
               (let [message (-> event .-target .-value)]
-                (send-message! message))))
-          (send-click-handler [event]
-            (let [message (-> event .-target .-parentNode (.querySelector "input") .-value)]
-              (send-message! message)))]
-    [:div
-     [:input {:type "text" :on-key-down enter-handler}]
-     [:button {:on-click send-click-handler} "Send"]]))
+                (send-message! message))))]
+    [:div {:class "footer"}
+     [:input {:class "input-box zero-fill"
+              :type "text"
+              :on-key-down enter-handler}]]))
 
 (defn root [{:keys [buffer send-message!] :as props}]
-  [:div
+  [:div {:class "container"}
    [output-console {:buffer buffer}]
    [input-line {:send-message! send-message!}]])
 
