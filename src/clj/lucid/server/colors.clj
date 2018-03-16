11ea792937b115c628b192d225a2ddaa7ce6e685
Dave Harlan <david.k.harlan@gmail.com>
2018-03-16 17:59:56 -0400

    Add light and dark variants of red, green, yellow, blue; purple, pink, gray and white

--- src/clj/lucid/server/colors.clj
+++ src/clj/lucid/server/colors.clj
@@ -6,11 +6,33 @@
 (def color-code-start \$)
 
 (def color-codes
-  {;; dark red
-   \r "\033[31m"
+  {;; dark red          light red
+   \r "\033[31m"        \R "\033[31;1m"
+
+   ;; dark green        light green
+   \g "\033[32m"        \G "\033[32;1m"
+
+   ;; dark yellow       light yellow
+   \y "\033[33m"        \Y "\033[33;1m"
+
+   ;; dark blue         light blue
+   \b "\033[34m"        \B "\033[34;1m"
+
+   ;; purple            pink
+   \p "\033[35m"        \P "\033[35;1m"
+
+   ;; dark cyan         light cyan
+   \c "\033[36m"        \C "\033[36;1m"
+
+   ;; gray              white
+   \w "\033[37m"        \W "\033[37;1m"
 
    ;; reset
-   \! "\033[0m"})
+   \! "\033[0m"
+
+   ;; identity
+   color-code-start
+   (str color-code-start)})
 
 (defn escape-color-codes [message]
   (string/join
